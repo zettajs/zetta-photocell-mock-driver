24b40d2ce196fb38d5ab90114e021eb483b503b5
Adam Magaluk <AdamMagaluK@gmail.com>
2016-03-11 12:26:37 -0700

    Revert state and transitions additions

--- example/apps/intermittent-connection.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(server) {
-  var photocellQuery = server.where({ type: 'photocell' });
-  server.observe([photocellQuery], function(photocell){
-    setInterval(function(){
-      (photocell.state == 'ready') ? photocell.call('make-not-ready') : photocell.call('make-ready');
-    }, 1000);
-  });
-};
--- example/server.js
+++ example/server.js
@@ -1,9 +1,6 @@
 var zetta = require('zetta');
 var Photocell = require('../index');
-var style = require('./apps/style');
-var intermittentConnection = require('./apps/intermittent-connection');
+
 zetta()
   .use(Photocell)
-  .use(style)
-  .use(intermittentConnection)
   .listen(1337);
